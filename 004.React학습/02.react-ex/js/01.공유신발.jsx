02fb5d15103987fefcd2b68522f07f0c09b70083
LEE WOOJIN <24864@naver.com>
2024-05-17 13:16:35 +0900

    react

--- 004.React학습/02.react-ex/js/01.공유신발.jsx
+++ 004.React학습/02.react-ex/js/01.공유신발.jsx
@@ -13,6 +13,10 @@ import guData from "./data/gu_data";
 
 // 함수형 컴포넌트는 첫글자 대문자인 함수키워드로 만든다
 function MainComponent() {
+  // [ 후크 상태관리 변수 셋팅! ] //
+  // 1. 리스트 / 상세보기 전환용 상태관리변수
+  const [viewList, setViewList] = React.useState(true);
+
   /****************************************** 
         [ 코드구성 ]
         1. 타이틀 : h1.tit
@@ -43,25 +47,93 @@ function MainComponent() {
       </div>
       {/* 4. 상품리스트박스 */}
       <div className="gwrap">
-        <ul>
-          {guData.map((v) => (
-            <li>
-              <a href="#">
-                <ol className="glist">
-                  <li>
-                    <img src={`./images/vans/vans_${v.idx}.jpg`} alt="신발" />
-                  </li>
-                  <li>{v.gname}</li>
-                  <li>가격 : {v.gprice}원</li>
-                </ol>
-              </a>
-            </li>
-          ))}
-        </ul>
+        {
+          // 상태관리변수 viewList값이 true이면 리스트보기
+          viewList ? <GoodsList viewDetail={setViewList} /> : <GoodsDetail backList={setViewList}/>
+          // false이면 상품 상세리스트 보기
+        }
       </div>
     </React.Fragment>
   );
 } ///////////// MainComponent 컴포넌트 //////
+
+// [ 상품리스트 서브 컴포넌트 : GoodsList ] //
+function GoodsList({ viewDetail }) {
+  // viewDetail - 부모컴포넌트가 전달해준 상태변수 backList를 업데이트하는 setViewList메서드
+  // 코드리턴구역/////
+  return (
+    <ul>
+      {
+        // 반복요소 li에 key 속성을 쓸것을 리액트는 필수적
+        // 어디에? 업데이트시 순번구분을 위함
+        // node.js개발환경에서는 안쓰면 에러
+        guData.map((v, i) => (
+          <li key={i}>
+            <a
+              href="#"
+              onClick={(e) => {
+                // a요소 기본이동막기
+                e.preventDefault();
+                // 상태변수 backList 업데이트
+                // setViewList메서드가 viewDetail로 들어옴
+                viewDetail(false);
+              }}
+            >
+              <ol className="glist">
+                <li>
+                  <img src={`./images/vans/vans_${v.idx}.jpg`} alt="신발" />
+                </li>
+                <li>{v.gname}</li>
+                <li>가격 : {v.gprice}원</li>
+              </ol>
+            </a>
+          </li>
+        ))
+      }
+    </ul>
+  );
+} /// GoodsList 컴포넌트 ///
+
+// [ 상품 상세보기 서브컴포넌트 : GoodsDetail ] ////
+function GoodsDetail({backList}) {
+  // backList - 부모컴포넌트가 전달해준 상태변수 backList를 업데이트하는 setViewList메서드
+  // 코드리턴구역 ////
+  return (
+    <ol
+      style={{ display: "flex", listStyle: "none", justifyContent: "center" }}
+    >
+      <li>
+        <img
+          src="./images/vans/vans_1.jpg"
+          alt="반스신발"
+          style={{ maxWidth: "100%" }}
+        />
+      </li>
+      <li style={{ lineHeight: "2", padding: "10px", textAlign: "left" }}>
+        상품명 : {guData[0].gname}
+        <br />
+        가격 : {guData[0].gprice} <br />
+        소재 : {guData[0].소재} <br />
+        색상 : {guData[0].색상} <br />
+        치수 : {guData[0].치수} <br />
+        제조자/수입자 : {guData[0]["제조자/수입자"]}
+        <br />
+        제조국 : {guData[0].제조국}
+        <br />
+        제조연월 : {guData[0].제조연월}
+        <br />
+        A/S 책임자와 전화번호 : <br />
+        {guData[0]["A/S 책임자와 전화번호"]}
+        <br />
+        Model : {guData[0].Model}
+        <br />
+        <div className="btnbx" style={{ textAlign: "right", padding: "15px" }}>
+          <button onClick={()=>{backList(true)}} style={{ fontSize: "24px" }}>리스트로 가기</button>
+        </div>
+      </li>
+    </ol>
+  );
+} //////// GoodsDetail 컴포넌트 ///
 
 // 메인 컴포넌트 출력하기 //////
 // ReactDOM.render(어쩌구,저쩌구);

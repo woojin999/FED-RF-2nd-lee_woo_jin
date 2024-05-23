c153d1b11e14b0dc4e73bb77d1ceba201e487d43
LEE WOOJIN <24864@naver.com>
2024-05-23 13:08:38 +0900

    react

--- 700.쉐도잉프로젝트/02.VOGUE_PJ/03.구현코드/js/main.js
+++ 700.쉐도잉프로젝트/02.VOGUE_PJ/03.구현코드/js/main.js
@@ -1,3 +1,26 @@
 // 보그 PJ 메인 JS - main.js
 
-//
+// 상단영역 불러오기
+import TopArea from "./components/TopArea";
+// 메인영역 불러오기
+import MainArea from "./components/MainArea";
+// 하단영역 불러오기
+import FooterArea from "./components/FooterArea";
+
+// [1] 메인 페이지 전체 레이아웃 로딩 컴포넌트 /////
+function Layout() {
+    // 코드 리턴구역
+  return (
+    <React.Fragment>
+      {/* 1. 상단영역 컴포넌트 */}
+      <TopArea />
+      {/* 2. 메인영역 컴포넌트 */}
+      <MainArea />
+      {/* 3. 하단영역 컴포넌트 */}
+      <FooterArea />
+    </React.Fragment>
+  );
+} ////Layout 컴포넌트 ///////////////
+
+// 메인페이지 전체출력하기
+ReactDOM.render(<Layout />, document.querySelector("#root"));

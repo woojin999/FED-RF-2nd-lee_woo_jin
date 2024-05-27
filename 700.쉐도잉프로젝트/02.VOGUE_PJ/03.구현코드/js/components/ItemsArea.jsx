0ca45391f7c0eeb498bb498836cef5f2994371d5
LEE WOOJIN <24864@naver.com>
2024-05-27 16:54:08 +0900

    vogue

--- 700.쉐도잉프로젝트/02.VOGUE_PJ/03.구현코드/js/components/ItemsArea.jsx
+++ 700.쉐도잉프로젝트/02.VOGUE_PJ/03.구현코드/js/components/ItemsArea.jsx
@@ -10,6 +10,26 @@ export default function ItemsArea({ catName }) {
   const selData = catData[catName];
   console.log(selData);
 
+  // 태그처리 구분 코드 생성함수
+  const makeCode = (data) => {
+    // console.log("배열?", Array.isArray(data));
+    // 배열 데이터는 태그 구성이 다름
+    // runway 카테고리만 다름
+    if (Array.isArray(data)) {
+      return (
+        <h2>
+          <small>{data[0]}</small>
+          <br />
+          {data[1]}
+        </h2>
+      );
+    } /// if ///
+    else {
+      // 배열이 아닌경우
+      return <h2>{data}</h2>;
+    } // else//
+  }; ///////makeCode 함수/////////
+
   return (
     <div id="main-area">
       {/* 데이터적용1: 최상위 클래스명 추가하기 */}
@@ -20,43 +40,34 @@ export default function ItemsArea({ catName }) {
           {/* 데이터적용2 : 제목넣기 */}
           <h2 className="cat-tit">{selData.제목}</h2>
           {/* 2-1-2. 서브메뉴(LNB:Local Navigation Bar) */}
-          
-          {// 메뉴가 "없음"이 아닐때만 배열돌아 출력함
-          selData.메뉴 != "없음" && (
-            <nav className="lnb">
-              <ul>
-                {selData.메뉴.map((v) => (
-                  <li>
-                    <a href="#">{v}</a>
-                  </li>
-                ))}
-              </ul>
-            </nav>
-          )}
+          {/* 데이터적용 3: 서브 메뉴 넣기 */}
+          {
+            // 메뉴가 "없음"이 아닐때만 배열돌아 출력함
+            selData.메뉴 != "없음" && (
+              <nav className="lnb">
+                <ul>
+                  {selData.메뉴.map((v) => (
+                    <li>
+                      <a href="#">{v}</a>
+                    </li>
+                  ))}
+                </ul>
+              </nav>
+            )
+          }
         </header>
         {/* 2-2. 카테고리 페이지 컨텐츠영역 */}
         <div className="cat-cont-area">
+          {/* 데이터적용4: 컨텐츠 타이틀 넣기 */}
           <section className="pt2">
-            <div className="cbx bgi bg1-1">
-              <h2></h2>
-            </div>
-            <div className="cbx bgi bg1-2">
-              <h2></h2>
-            </div>
-            <div className="cbx bgi bg1-3">
-              <h2></h2>
-            </div>
+            <div className="cbx bgi bg1-1">{makeCode(selData.타이틀[0])}</div>
+            <div className="cbx bgi bg1-2">{makeCode(selData.타이틀[1])}</div>
+            <div className="cbx bgi bg1-3">{makeCode(selData.타이틀[2])}</div>
           </section>
           <section className="pt2">
-            <div className="cbx bgi bg2-1">
-              <h2></h2>
-            </div>
-            <div className="cbx bgi bg2-2">
-              <h2></h2>
-            </div>
-            <div className="cbx bgi bg2-3">
-              <h2></h2>
-            </div>
+            <div className="cbx bgi bg2-1">{makeCode(selData.타이틀[3])}</div>
+            <div className="cbx bgi bg2-2">{makeCode(selData.타이틀[4])}</div>
+            <div className="cbx bgi bg2-3">{makeCode(selData.타이틀[5])}</div>
           </section>
         </div>
       </main>

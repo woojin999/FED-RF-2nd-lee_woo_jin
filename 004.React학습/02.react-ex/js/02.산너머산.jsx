a5f7685a1b7ad30c43f5e89deea0c493d69fc544
LEE WOOJIN <24864@naver.com>
2024-05-28 17:37:31 +0900

    js

--- 004.React학습/02.react-ex/js/02.산너머산.jsx
+++ 004.React학습/02.react-ex/js/02.산너머산.jsx
@@ -1,5 +1,10 @@
 // 산너머산 메인 컴포넌트
 
+// 이야기 서브 컴포넌트 불러오기
+import 이야기 from "./components/story";
+// 컨텍스트 프로바이더 불러오기
+import { 누구냐 } from "./components/provider";
+
 /********************************************** 
     1. props로 데이터를 전달하여 제목출력하기 
     -> props Down으로 데이터를 하위 컴포넌트에 전달
@@ -36,3 +41,68 @@ function MyEnd({ ee, ff, cdata }) {
 
 // 화면출력 ////////////
 ReactDOM.render(<MyHome />, document.querySelector("#root1"));
+
+/****************************************************** 
+    2. 컨텍스트 프로바이더를 사용하여 산정보 셋팅하기
+******************************************************/
+// 산전체 박스CSS
+const mtBoxCss = {
+  position: "relative",
+  padding: "20px",
+  border: "10px dotted skyblue",
+  borderRadius: "10px",
+  width: "60%",
+  margin: "20px auto",
+  textAlign: "center",
+};
+// 산정보박스CSS
+const mtInfoBoxCss = {
+  position: "absolute",
+  width: "50%",
+  bottom: "105px",
+  left: "20px",
+  padding: "15px",
+  fontSize: "16px",
+  color: "#fff",
+  textShadow: "0 0 5px #000",
+  textAlign: "left",
+  borderRadius: "20px",
+  backgroundColor: "rgb(0 0 0 / .4)",
+};
+
+// 메인 컴포넌트 ////////////
+function 큰집() {
+  // 리액트 상태변수 셋팅
+  const [mtName, setMtName] = React.useState("백두산");
+  // 코드 리턴구역 //
+  return (
+    // 형식: <프로바이더변수.provider value={{변수,함수}}
+    <누구냐.Provider value={{mtName,setMtName,mtBoxCss,mtInfoBoxCss}}>
+      <할아버지 />
+    </누구냐.Provider>
+  );
+} /////////// 큰집 /////////////
+
+//// 서브컴포넌트 //////
+function 할아버지() {
+  // 코드 리턴구역 //
+  return <아버지 />;
+} /////////// 할아버지 /////////////
+
+function 아버지() {
+  // 코드 리턴구역 //
+  return <아들 />;
+} /////////// 아버지 /////////////
+
+function 아들() {
+  // 코드 리턴구역 //
+  return <손녀 />;
+} /////////// 아들 /////////////
+
+function 손녀() {
+  // 코드 리턴구역 //
+  return <이야기 />;
+} /////////// 손녀 /////////////
+
+//// 산 정보 내용 출력하기 ///
+ReactDOM.render(<큰집 />, document.querySelector("#root2"));

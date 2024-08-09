92848adfd5bdbd5000924c0ab8668df11d935ff0
LEE WOOJIN <24864@naver.com>
2024-08-09 13:46:32 +0900

    v

---
+++ 007.VueJS학습/05.SPA구성연습/city-vue-app/src/App.vue
@@ -0,0 +1,36 @@
+<!-- 템플릿구역 -->
+<template>
+  <div id="app">
+    <!-- <TopArea></TopArea> -->
+    <TopArea/>
+    <MainArea/>
+    <FooterArea/>
+  </div>
+</template>
+
+<!-- 스트립트 -->
+<script>
+import TopArea from './components/TopArea.vue';
+import MainArea from './components/MainArea.vue';
+import FooterArea from "./components/FooterArea.vue";
+
+export default {
+    name: 'App',
+    components: { TopArea,MainArea,FooterArea }
+}
+</script>
+
+<!-- 스타일구역 -->
+<style>
+/* 최상위 요소 */
+#app {
+  max-width: 500px;
+  margin: 0 auto;
+  /* font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px; */
+}
+</style>
